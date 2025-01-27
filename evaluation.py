759d669c38984f2779a6a3086f5010865df60193
Dennis Mustafic <dennismustafic@gmail.com>
2025-01-27 09:51:07 +0100

    Made the EvaluationModel class more universal

--- evaluation.py
+++ evaluation.py
@@ -13,12 +13,24 @@ class EvaluateModel:
             self, 
             model_path: Path,
             save_path: Path = Path("results"),
-            batch_size: int = 16):
+            batch_size: int = 16,
+            dataset_columns: list = ["non_gendered", "gendered"],
+            prefix: str = "Bringe den Satz in eine ungegenderte Form: "
+            ):
+        """
+        :param model_path: The path to the trained model.
+        :param save_path: The path to save the results.
+        :param batch_size: The batch size for generating predictions.
+        :param dataset_columns: The columns of the dataset. The first column should be the target sentence, the second the input sentence.
+        :param prefix: The prefix to add to the input sentence, being for example "Übersetze den Satz in XY: ".
+        """
 
         self.model = T5ForConditionalGeneration.from_pretrained(model_path)
         self.tokenizer = T5Tokenizer.from_pretrained(model_path)
         self.save_path = save_path / "results.csv"
         self.batch_size = batch_size
+        self.dataset_columns = dataset_columns
+        self.prefix = prefix
 
     def eval_with_dataset(self, test_data_path: Path = TEST_DATA_PATH) -> None:
         """
@@ -31,7 +43,9 @@ class EvaluateModel:
             raise FileNotFoundError(f"Cannot find evaluation data at {test_data_path}.")
 
         test_data_df = pd.read_csv(test_data_path)
-        test_data_df = test_data_df[["non_gendered", "gendered"]]
+        test_data_df = test_data_df[self.dataset_columns]
+        test_data_df = test_data_df.dropna() # Drop rows with missing values
+
         test_dataset = Dataset.from_pandas(test_data_df)
         test_dataset = test_dataset.map(self.__preprocess_data, batched=True)
 
@@ -42,18 +56,20 @@ class EvaluateModel:
             predictions.extend(batch_predictions)
 
         results = pd.DataFrame({
-            "non_gendered": test_data_df["non_gendered"],
-            "gendered": test_data_df["gendered"],
+            self.dataset_columns[0]: test_data_df[self.dataset_columns[0]],
+            self.dataset_columns[1]: test_data_df[self.dataset_columns[1]],
             "predicted": predictions
         })
 
         results.to_csv(self.save_path, index=False)
 
-    def __preprocess_data(self, examples) -> dict:
+    def __preprocess_data(self, examples: list) -> dict:
         """
         Preprocess the data for the model.
+        :param examples: The examples to preprocess.
+        :return: The preprocessed examples.
         """
-        inputs = examples["gendered"]
+        inputs = [self.prefix + sentence for sentence in examples[self.dataset_columns[1]]]
         model_inputs = self.tokenizer(
             inputs, max_length=512, truncation=True, padding="max_length", return_tensors="pt")
         return model_inputs
@@ -62,7 +78,7 @@ class EvaluateModel:
         """
         Generate predictions for the model.
         """
-        inputs = self.tokenizer(examples["gendered"], return_tensors="pt", max_length=512, truncation=True, padding="max_length")
+        inputs = self.tokenizer(examples[self.dataset_columns[0]], return_tensors="pt", max_length=512, truncation=True, padding="max_length")
         input_ids = inputs.input_ids
         attention_mask = inputs.attention_mask
 
@@ -88,6 +104,7 @@ class EvaluateModel:
 
 
 if __name__ == "__main__":
+    # First experiment: translation from gendered to non-gendered sentences
     experiment_path = Path("experiments", "flan_t5_finetuning_correlaid", "2025-01-09_10-37-49")
 
     model_path = Path(experiment_path, "model")
@@ -95,7 +112,26 @@ if __name__ == "__main__":
 
     eval_model = EvaluateModel(model_path, save_path)
 
-    test_sentence = "Bringe den Satz in eine ungenderte Form: Die Lehrperson ist cool."
+    test_sentence = "Bringe den Satz in eine ungenderte Form: Die Lehrer*innen sind cool."
     print("Predicted sentence:", eval_model.eval_with_sentence(test_sentence))
 
-    eval_model.eval_with_dataset()
+#    eval_model.eval_with_dataset()
+
+
+    # Second experiment: translation from gendered sentences into inclusive form
+    experiment_path = Path("experiments", "flan_t5_finetuning_inclusive_form", "2025-01-26_19-33-13")
+    model_path = Path(experiment_path, "model")
+    save_path = Path(experiment_path, "results")
+
+    eval_model = EvaluateModel(
+        model_path, 
+        save_path, 
+        dataset_columns=["gendered", "enhanced"], 
+        prefix = "Bringe den Satz in eine inklusive Form: "
+        )
+
+    test_sentence = "Bringe den Satz in eine inklusive Form: Die Lehrer*innen sind cool."
+    print("Predicted sentence:", eval_model.eval_with_sentence(test_sentence))
+
+    test_data_path = Path("long_form_data", "test.csv")
+    eval_model.eval_with_dataset(test_data_path)
